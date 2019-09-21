2cca7d0378f29dc853609b68c53efafc4b7d7bd0
Raywire <ryanwire@outlook.com>
2019-09-21 18:16:54 +0300

    Creating a class

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,7 +7,9 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            var book = new Book();
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
             var result = 0.0;
@@ -18,14 +20,6 @@ namespace GradeBook
             System.Console.WriteLine(result);
             result /= grades.Count;
             System.Console.WriteLine($"The average grade is {result:N2}");
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello {args[0]}!");
-            }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
         }
     }
 }
