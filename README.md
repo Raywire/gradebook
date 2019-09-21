1806fe76ef3b99031fb4784c12e3f22c0503c2a8
Raywire <ryanwire@outlook.com>
2019-09-21 17:23:03 +0300

    Looping through arrays

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ### Prerequisites
 -  [.NET Core SDK](https://dotnet.microsoft.com/download)
+
+### Description
+We need an electronic grade book to read the scores of an individual student and then compute some simple statistics from the scores.
+The grades are entered as floating point numbers from 0 to 100, and the statistics should show us the highest grade, the lowest grade and the average grade.
 ### Run the project
 ```bash
 dotnet run
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,9 +6,26 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0) {
+            double x = 34.1;
+            double y = 10.3;
+
+            var result = x + y;
+            System.Console.WriteLine(result);
+
+            var numbers = new [] { 12.7, 10.3, 6.11, 4.1 };
+
+            var result2 = 0.0;
+            foreach(var number in numbers) 
+            {
+                result2 += number;
+            }
+            System.Console.WriteLine(result2);
+            if (args.Length > 0)
+            {
                 Console.WriteLine($"Hello {args[0]}!");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("Hello!");
             }
         }
