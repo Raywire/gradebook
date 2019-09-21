c0f0d18d83fd929232afb8c59341c16bd4a5418a
Raywire <ryanwire@outlook.com>
2019-09-21 17:54:12 +0300

    Computing and formatting the result

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,12 +10,14 @@ namespace GradeBook
             List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
-            var result2 = 0.0;
+            var result = 0.0;
             foreach(var number in grades) 
             {
-                result2 += number;
+                result += number;
             }
-            System.Console.WriteLine(result2);
+            System.Console.WriteLine(result);
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is {result:N2}");
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]}!");
