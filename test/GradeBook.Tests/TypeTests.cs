308980be2b9ce709004a29f6beb83a125b29a30b
Raywire <ryanwire@outlook.com>
2019-09-22 11:25:49 +0300

    Passing Parameters by value

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,35 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void CSharpIsPassByValue()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book = new Book(name);
+            book.Name = name;
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        private void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {
             var book1 = GetBook("Book 1");
@@ -24,9 +53,9 @@ namespace GradeBook.Tests
             Assert.Same(book1, book2);
         }
 
-    private Book GetBook(string name)
-    {
-        return new Book(name);
+        private Book GetBook(string name)
+        {
+            return new Book(name);
+        }
     }
-  }
 }
