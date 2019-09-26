0315a77f002dd7a85e9933e8e32b8376fcad9424
Raywire <ryanwire@outlook.com>
2019-09-26 20:58:48 +0300

    Object-oriented Programming with C#: Refactoring Statistics

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -48,7 +48,20 @@ namespace GradeBook
 
     public override Statistics GetStatistics()
     {
-      throw new NotImplementedException();
+      var result = new Statistics();
+
+      using(var reader = File.OpenText($"{Name}.txt"))
+      {
+        var line = reader.ReadLine();
+        while(line != null)
+        {
+          var number = double.Parse(line);
+          result.Add(number);
+          line = reader.ReadLine();
+        }
+      }
+
+      return result;
     }
   }
 
@@ -108,40 +121,10 @@ namespace GradeBook
     public override Statistics GetStatistics()
     {
       var result = new Statistics();
-      result.Average = 0.0;
-      result.Highest = double.MinValue;
-      result.Lowest = double.MaxValue;
 
       for (var index = 0; index < grades.Count; index += 1)
       {
-        result.Lowest = Math.Min(grades[index], result.Lowest);
-        result.Highest = Math.Max(grades[index], result.Highest);
-        result.Average += grades[index];
-      }
-
-      result.Average /= grades.Count;
-
-      switch(result.Average)
-      {
-        case var d when d >= 90.0:
-          result.Letter = 'A';
-          break;
-
-        case var d when d >= 80.0:
-          result.Letter = 'B';
-          break;
-
-        case var d when d >= 70.0:
-          result.Letter = 'C';
-          break;
-
-        case var d when d >= 60.0:
-          result.Letter = 'D';
-          break;
-
-        default:
-          result.Letter = 'F';
-          break;
+        result.Add(grades[index]);
       }
 
       return result;
--- src/GradeBook/Statistics.cs
+++ src/GradeBook/Statistics.cs
@@ -1,10 +1,58 @@
+using System;
+
 namespace GradeBook
 {
   public class Statistics
   {
-    public double Average;
+    public double Average
+    {
+      get
+      {
+        return Sum / Count;
+      }
+    }
     public double Highest;
     public double Lowest;
-    public char Letter;
+    public char Letter
+    {
+      get
+      {
+        switch (Average)
+        {
+          case var d when d >= 90.0:
+            return 'A';
+
+          case var d when d >= 80.0:
+            return 'B';
+
+          case var d when d >= 70.0:
+            return 'C';
+
+          case var d when d >= 60.0:
+            return 'D';
+
+          default:
+            return 'F';
+        }
+      }
+    }
+    public double Sum;
+    public int Count;
+
+    public void Add(double number)
+    {
+      Sum += number;
+      Count += 1;
+      Lowest = Math.Min(number, Lowest);
+      Highest = Math.Max(number, Highest);
+    }
+
+    public Statistics()
+    {
+      Count = 0;
+      Sum = 0.0;
+      Highest = double.MinValue;
+      Lowest = double.MaxValue;
+    }
   }
 }
