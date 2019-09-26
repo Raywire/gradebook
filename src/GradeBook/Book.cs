81f7be04e832849addc89223a56752b17d93e295
Raywire <ryanwire@outlook.com>
2019-09-26 19:04:35 +0300

    Object-oriented Programming with C#: Defining an Abstract Class Polymorphism

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -4,12 +4,21 @@ using System.Collections.Generic;
 namespace GradeBook
 {
   public delegate void GradeAddedDelegate(object sender, EventArgs args);
+  
+  public abstract class Book : NamedObject
+  {
+    public Book(string name) : base(name)
+    {
+    }
 
-  public class Book : NamedObject
+    public abstract void AddGrade(double grade);
+  }
+
+  public class InMemoryBook : Book
   {
     //constructor
     // base() accessing constructor of the base class
-    public Book(string name) : base(name)
+    public InMemoryBook(string name) : base(name)
     {
       grades = new List<double>();
       Name = name;
@@ -37,7 +46,7 @@ namespace GradeBook
       }
     }
 
-    public void AddGrade(double grade)
+    public override void AddGrade(double grade)
     {
       if(grade <= 100 && grade >= 0)
       {
